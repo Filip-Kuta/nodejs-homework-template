69c34052c807d21995b51a6b1a65569285835d1f
Filip-Kuta <114823329+Filip-Kuta@users.noreply.github.com>
2023-09-28 08:16:20 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -4,54 +4,81 @@ const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "contacts.json");
 
-const writeContact = async (contacts) =>
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+const writeContact = async (contacts) => {
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    console.error("Błąd podczas zapisywania kontaktów:", error);
+  }
+};
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Błąd podczas odczytywania kontaktów:", error);
+    return [];
+  }
 };
 
 const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  const result = contacts.find((contact) => contact.id === contactId);
-
-  return result || null;
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((contact) => contact.id === contactId);
+    return result || null;
+  } catch (error) {
+    console.error("Błąd podczas pobierania kontaktu:", error);
+    return null;
+  }
 };
 
 const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await writeContact(contacts);
+    return result;
+  } catch (error) {
+    console.error("Błąd podczas usuwania kontaktu:", error);
     return null;
   }
-  const [result] = contacts.splice(index, 1);
-  await writeContact(contacts);
-  return result;
 };
 
 const addContact = async (body) => {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    ...body,
-  };
-  contacts.push(newContact);
-  await writeContact(contacts);
-
-  return newContact;
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      ...body,
+    };
+    contacts.push(newContact);
+    await writeContact(contacts);
+    return newContact;
+  } catch (error) {
+    console.error("Błąd podczas dodawania kontaktu:", error);
+    return null;
+  }
 };
 
 const updateContact = async (contactId, body) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    contacts[index] = { contactId, ...body };
+    await writeContact(contacts);
+    return contacts[index];
+  } catch (error) {
+    console.error("Błąd podczas aktualizacji kontaktu:", error);
     return null;
   }
-  contacts[index] = { contactId, ...body };
-  await writeContact(contacts);
-  return contacts[index];
 };
 
 module.exports = {
